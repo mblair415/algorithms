9ee1fe903789f59f8a3282718fc65bcc268add8a
mblair415 <mblair415@gmail.com>
2018-09-28 10:53:20 -0700

    more details in problem description

--- array/matrix/latticePaths.js
+++ array/matrix/latticePaths.js
@@ -18,6 +18,37 @@ input: 2, 3
  1 1 1 1
  1 2 3 4
  1 3 6 10  <--- this is the answer
+
+
+ note that there are N+1 and M+1 positions.
+
+basic problem logic:
+
+There is one way to be at position 0,0.
+There is one way to be at each sequential position on the 0 row.
+ This is because all you can do is move right.  There is no way to move up
+ to get back up to the 0 row to find a second way to reach any position here,
+ and you also cannot move to the left (because it isn't allowed by the rules).
+There is one way to be at each of the sequential positions in the 0 column.
+ This is because all you can do is move down.  You cannot move to the left
+ to reach this row again, once you leave it you're done with it.
+If there is 1 way to be at position 0,1 and 1 way to be at position 1,0,
+then there 1,0 and 0,1 ways to reach position 1,1 because you can only reach this
+position from those two coordinates.
+This root logic, position left and position above, falls apart when you're along
+the 0 row or columns.
+ This logic is repaired when you realize that there is no possible way to reach
+ a position in -1 row or -1 column.  So, any position off the grid has 0
+ possible ways to reach that position. 1+0 == 1, and this is the number of
+ ways to reach each position in the 0 column or 0 row.
+
+This problem also follows pascal's triangle.
+          1
+       1     1
+     1    2    1
+   1   3    3    1
+ 1   4   6    4     1
+1   5  10   10   5     1
 */
 
 function latticePathsPureRecursion(n, m = n) {
