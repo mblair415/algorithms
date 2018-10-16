f58aa0daf2ce993f4e78712cc388069625e5b271
mblair415 <mblair415@gmail.com>
2018-10-15 19:35:33 -0700

    graph traversal

--- array/matrix/pascalsTriangle.js
+++ array/matrix/pascalsTriangle.js
@@ -8,20 +8,26 @@
   1,4,6,4,1
  1,5,10,10,5,1
 
+input: 3
+output: [[1],[1,1],[1,2,1]]
 
 leetcode result:
  Runtime: 52 ms, faster than 92.38% of JavaScript online submissions for Pascal's Triangle.
  */
 function pascalsTriangle(numRows) {
   if (numRows == 0) return [];
-  let newTier
-  let result = [[1]]
+  let newTier;
+  let result = [[1]];
+  let previousRow;
+
   while (result.length < numRows) {
-    let previousRow = result[result.length - 1];
+    previousRow = result[result.length - 1];
     newTier = [1];
+
     for (let i = 0; i < previousRow.length - 1; i++) {
       newTier.push(previousRow[i] + previousRow[i + 1]);
     }
+    
     newTier.push(1);
     result.push(newTier);
   }
---
+++ graph/traversals.js
@@ -0,0 +1,47 @@
+/*
+Breath first traversal in a graph
+*/
+
+function bfs(vertex){
+  let visited = new Set().add(vertex.id),
+    queue = [vertex], // would be better with a linked list
+    result = [vertex.id],
+    current;
+
+  while (queue.length > 0) {
+    current = queue.shift();
+
+    current.edges.forEach( edge => {
+      if (!visited.has(edge.id)) {
+        queue.push(edge);
+        visited.add(edge.id);
+        result.push(edge.id)
+      }
+    });
+  }
+  return result;
+}
+
+/*
+Depth first traversal in a graph
+*/
+
+function dfs(vertex){
+  let stack = [vertex],
+    visited = new Set().add(vertex.id),
+    result = [],
+    current;
+
+  while (stack.length > 0) {
+    current = stack.pop();
+
+    current.edges.forEach( edge => {
+      if (!visited.has(edge.id)) {
+        visited.add(edge.id);
+        stack.push(edge);
+      }
+    });
+    result.push(current.id)
+  }
+  return result;
+}
